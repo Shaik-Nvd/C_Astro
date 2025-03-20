1b9d1f99e073999d61cda570f09fd74f98d6bc96
nvd_shaik <mail2nvd@gmail.com>
2025-03-20 06:57:06 +0530

    Deploy updated build to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   integrations: [mdx()],
+  site: "https://shaik-nvd.github.io/C_Astro/",
+  base: "/C_Astro/",
+  output: "static",
 });
